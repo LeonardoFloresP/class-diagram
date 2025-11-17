17ad717826891c34515cbac78023cda02fdbc917
Leonardo Flores <a01784267@tec.mx>
2025-11-17 12:12:16 -0600

    Adjust Book.cpp to use Media base class

--- Book.cpp
+++ Book.cpp
@@ -1,21 +1,22 @@
 #include "Book.h"
+#include <iostream>
+
+using namespace std;
+
 
 Book::Book(string t, string a, string i, int year)
-    : title(t), author(a), isbn(i), publicationYear(year) {}
+    : Media(t), author(a), isbn(i), publicationYear(year) {}
 
 string Book::getIsbn() const {
     return isbn;
 }
 
-string Book::getTitle() const {
-    return title;
-}
 
 void Book::printInfo() const {
-    cout << "Title: " << title
+
+    cout << "Book Title: " << title
          << " | Author: " << author
          << " | ISBN: " << isbn
          << " | Year: " << publicationYear
          << endl;
 }
-
