4d6618bf26bbcdf7743479510b71ee03b315d519
Leonardo Flores <a01784267@tec.mx>
2025-11-17 12:10:48 -0600

    Feature: Make Book inherit from Media

--- Book.h
+++ Book.h
@@ -1,24 +1,28 @@
 #ifndef BOOK_H
 #define BOOK_H
 
+
+#include "Media.h" 
 #include <string>
 #include <iostream>
 
 using namespace std;
 
-class Book {
+
+class Book : public Media {
 private:
-    string title;
+
     string author;
     string isbn;
     int publicationYear;
 
 public:
-    Book(string t, string a, string i, int year);
+
+    Book(string t, string a, string i, int year) : Media(t), author(a), isbn(i), publicationYear(year) {} 
+    
     string getIsbn() const;
-    string getTitle() const;
-    void printInfo() const;
+
+    void printInfo() const override; 
 };
 
 #endif // BOOK_H
-
