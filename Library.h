85421161d56aeff30ff6d8a3d1ea344e45f6c412
Leonardo Flores <a01784267@tec.mx>
2025-11-17 12:17:04 -0600

    Refactor Library class to use Media pointers

--- Library.h
+++ Library.h
@@ -2,6 +2,8 @@
 #define LIBRARY_H
 
 #include <vector>
+#include <string>
+#include "Media.h"    
 #include "Book.h"
 #include "Member.h"
 #include "Loan.h"
@@ -10,25 +12,35 @@ using namespace std;
 
 class Library {
 private:
-    vector<Book> books;
+    vector<Media*> items; 
     vector<Member> members;
     vector<Loan> loans;
 
-    Book* findBook(const string& isbn);
-    Member* findMember(const string& memberId);
+
+    Media* findItem(const string& query); 
+    Member* findMember(const string& memberId); 
     int countActiveLoansForMember(const string& memberId) const;
-    bool isBookAvailable(const string& isbn) const;
+    bool isItemAvailable(const string& query) const; 
     Loan* findActiveLoan(const string& memberId, const string& isbn);
 
 public:
-    void addBook(const Book& book);
+
+    void addItem(Media* item); 
+    void listItems() const;     
+
+
+    bool removeBook(const string& isbn);
+    bool removeMember(const string& memberId);
+    vector<Book> searchBook(const string& query) const;
+
+  
     void addMember(const Member& member);
-    void listBooks() const;
     void listMembers() const;
     void listLoans(bool onlyActive = false) const;
     bool borrowBook(const string& memberId, const string& isbn, const string& borrowDate);
     bool returnBook(const string& memberId, const string& isbn, const string& returnDate);
+    
+   
 };
 
 #endif // LIBRARY_H
-
